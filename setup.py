70755f5205f6d02290ddced7bda73fe56ed677dc
Esteban Munoz <emunozh@gmail.com>
2017-10-27 15:57:24 +0200

    add yml file for rtd

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = [
     'matplotlib',
     'seaborn',
     'Theano',
-    'pymc3==3.0',
+    'pymc3',
     'rpy2',
     'ipfn'
 ]
@@ -61,7 +61,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
