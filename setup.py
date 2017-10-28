cebd8e2c3f69e596bfe41ece508e6df32a0f0ff7
Esteban Munoz <emunozh@gmail.com>
2017-10-28 11:59:49 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,28 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 with open(path.join(here, 'VERSION')) as f:
     version = f.read().strip()
 
-install_requires = [
-    'pandas',
-    'numpy',
-    'scipy',
-    'statsmodels',
-    'matplotlib',
-    'seaborn',
-    'Theano',
-    'pymc3',
-    'ipfn'
-]
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    install_requires = [
+        'pandas',
+        'numpy',
+        'scipy',
+        'statsmodels',
+        'matplotlib',
+        'seaborn',
+        'Theano',
+        'pymc3',
+        'ipfn',
+        'rpy2',
+    ]
+    
+    setup_requires=[
+        "setuptools_git >= 1.0",
+    ],
+
+else:
+    install_requires = []
+    setup_requires = []
 
 setup(
     name='umsm',
@@ -64,33 +75,26 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     
-    on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-    if not on_rtd:
-        # You can just specify the packages manually here if your project is
-        # simple. Or you can use find_packages().
-        packages=find_packages(),
-        include_package_data=True,
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(),
+    # include_package_data=True,
 
-        # List run-time dependencies here.  These will be installed by pip when
-        # your project is installed. For an analysis of "install_requires" vs pip's
-        # requirements files see:
-        # https://packaging.python.org/en/latest/requirements.html
-        install_requires=install_requires,
-
-        setup_requires=[
-            "setuptools_git >= 1.0",
-        ],
-
-        # List additional groups of dependencies here (e.g. development
-        # dependencies). You can install these using the following syntax,
-        # for example:
-        # $ pip install -e .[dev,test]
-        # extras_require={
-            # 'dev': ['nose', 'flake8'],
-            # 'test': ['nose', 'flake8'],
-            # 'gdal': ['GDAL', 'Shapely>=1.5.13'],
-        # },
-    else:
-        install_requires = []
-        setup_requires = []
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+        # 'dev': ['nose', 'flake8'],
+        # 'test': ['nose', 'flake8'],
+        # 'gdal': ['GDAL', 'Shapely>=1.5.13'],
+    # },
+    
 )
