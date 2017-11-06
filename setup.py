02e7c85f699b809bff6501ef0414e9dc10a4bf3b
Esteban Munoz <emunozh@gmail.com>
2017-11-06 10:23:36 +0100

    fix #3: install urbanmetabolism lib

--- setup.py
+++ setup.py
@@ -78,8 +78,8 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    # packages=find_packages(),
-    # include_package_data=True,
+    packages=find_packages(),
+    include_package_data=True,
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
