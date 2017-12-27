b85997448c170efc7cb6f3765634a1f892198663
Esteban Munoz <emunozh@gmail.com>
2017-12-27 13:21:01 +0100

    fix test path to match new examples

--- test_um.py
+++ test_um.py
@@ -9,7 +9,7 @@ Fri 13 Oct 2017 02:05:09 PM CEST
 from urbanmetabolism.population.model import TableModel
 
 verbose = True
-census_file = 'docs/examples/data/benchmarks_be_year_bias3_climate.csv'
+census_file = 'docs/example_ph/data/benchmarks_be_year_bias3_climate.csv'
 
 tm = TableModel(census_file = census_file, verbose = verbose)
 
